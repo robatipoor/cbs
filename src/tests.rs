988901fb2c7bbc6abb7f9b9e0fd7a41cf5eab1ff
mahdi <mahdi.robatipoor@gmail.com>
2019-06-18 15:08:44 +0430

    remove test_run

--- src/tests.rs
+++ src/tests.rs
@@ -6,7 +6,6 @@ use std::process::Command;
 
 #[test]
 fn command_test() {
-    run_test(|| {
         Command::cargo_bin(env!("CARGO_PKG_NAME"))
             .unwrap()
             .arg("-c")
@@ -34,29 +33,4 @@ fn command_test() {
             .unwrap()
             .stdout;
         assert_eq!(std::str::from_utf8(&out).unwrap().trim(), "");
-    });
-}
-
-fn run_test<T>(test: T)
-where
-    T: FnOnce() -> () + panic::UnwindSafe,
-{
-    std::env::set_var("RUST_LOG", "debug");
-    let _ = Command::cargo_bin(env!("CARGO_PKG_NAME"))
-        .unwrap()
-        .arg("-s")
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-    let _result = panic::catch_unwind(|| {
-        test();
-    });
-    let _ = Command::cargo_bin(env!("CARGO_PKG_NAME"))
-        .unwrap()
-        .arg("-k")
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
 }
